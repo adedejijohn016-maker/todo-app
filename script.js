c5c6999c533de13bbe2909e8345e06d9d7ec66d0
unknown <adedejijohn016@gmail.com>
2026-07-26 18:15:36 +0100

    second commit

--- script.js
+++ script.js
@@ -16,9 +16,76 @@ function renderTodos() {
 
   // render each item
   todos.forEach((todo) => {
-    const li = document.createElement('li');
+    const li = document.createElement('li'); 
     if (todo.completed) li.classList.add('completed');
+
+
+    li.innerHTML = `
+    <input type="checkbox" ${todo.completed ? 'checked' : ''} data-id="${todo.id}">
+    <span class="todo-text">${todo.text}</span>
+    <button class="delete-btn" data-id="${todo.id}">x</button>`;
+
+    todoList.appendChild(li);
   });
+
+// UPDATE THE FOLDER STAT
+const activeCount = todos.filter((t) => !t.completed).length; 
+taskCount.textContent (`you have ${activeCount} task${activeCount !== 1 ? 's' : ''} left`);
+
+// SAVE TO LOCAL STORAGE
+localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-// renderTodos()
+// ACTIONS
+const addTodo = (text) => {
+  const newTodo = {
+    id: Date.now(),
+    text: text.trim(),
+    completed: false,
+  };
+  todos.push(newTodo);
+  renderTodos();  
+}
+
+const toggleTodo = (id) => {
+  todos = todos.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo);
+  renderTodos();
+}
+
+const deleteTodo = (id) => {
+  todos = todos.filter((todo) => todo.id !== id);
+  renderTodos();
+}
+
+function clearCompleted() {
+  todos = todos.filter((todo) => !todo.completed);
+  renderTodos();
+}
+
+// HELPER FOR HTML INJECTION ATTACKS
+function escapeHTML(str) {
+  return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
+}
+
+  // EVENT LISTENERS
+  todoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!todoInput.value.trim()) return;
+    addTodo(todoInput.value);
+    todoInput.value = '';
+  });
+
+
+  // for delete and toggle
+  todoList.addEventListener('click', (e) => {
+   const id = Number(e.target.dataset.id);
+   if (!id) return;
+
+   if (e.target.matches('input[type="checkbox"]')) {
+      toggleTodo(id);
+    } else if (e.target.classList.contains('delete-btn')) {
+      deleteTodo(id);
+    }
+  });
+
+  clearBtn.addEventListener('click', clearCompleted);
